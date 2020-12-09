1789b96ed1f0ced0ea8d4ababc50088cf8b2f971
lloydmeta <lloydmeta@gmail.com>
2020-12-09 19:17:19 +0900

    part 2 works
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- src/day_09.rs
+++ src/day_09.rs
@@ -8,31 +8,60 @@ pub fn run() -> Result<()> {
     println!("Input: {}", INPUT);
     let numbers = parse(INPUT);
 
-    println!(
-        "Solution 1: {:?}",
-        find_preamble_error(&numbers, 25).first()
-    );
+    let solution_1 = find_preamble_error(&numbers, 25);
+    println!("Solution 1: {:?}", solution_1);
 
+    let solution_2 = find_min_max_sums_in_first_window_adding_to(&numbers, solution_1.unwrap_or(0));
+    println!("Solution 2: {:?}", solution_2);
     Ok(())
 }
 
-fn find_preamble_error(v: &[usize], preamble_size: usize) -> Vec<usize> {
-    (preamble_size..v.len() - 1)
+fn find_preamble_error(v: &[usize], preamble_size: usize) -> Option<usize> {
+    v.windows(preamble_size + 1)
         .into_iter()
-        .filter_map(|idx| {
-            let number = v[idx];
-            let preamble = v[idx - preamble_size..idx].into_iter().copied();
-            let first_sum_in_preamble_to_num = preamble
-                .combinations(2)
-                .find(|v| v.iter().sum::<usize>() == number);
-            if first_sum_in_preamble_to_num.is_none() {
-                // nothing in the preamble adds to this number
-                Some(number)
+        .filter_map(|slice| {
+            if let Some(number) = slice.last() {
+                let preamble = slice.iter().take(preamble_size).copied();
+                let first_sum_in_preamble_to_num = preamble
+                    .combinations(2)
+                    .find(|v| v.iter().sum::<usize>() == *number);
+                if first_sum_in_preamble_to_num.is_none() {
+                    // nothing in the preamble adds to this number
+                    Some(*number)
+                } else {
+                    None
+                }
             } else {
                 None
             }
         })
-        .collect()
+        .next()
+}
+
+fn find_min_max_sums_in_first_window_adding_to(v: &[usize], target: usize) -> Option<usize> {
+    let valid_numbers: Vec<_> = v.iter().filter(|i| **i <= target).copied().collect();
+    (2..valid_numbers.len())
+        .into_iter()
+        .flat_map(|window_size| {
+            let sums_to_target: Vec<_> = valid_numbers
+                .as_slice()
+                .windows(window_size)
+                .filter_map(|window| {
+                    let sum = window.iter().sum::<usize>();
+                    if sum == target {
+                        window
+                            .iter()
+                            .min()
+                            .zip(window.iter().max())
+                            .map(|(min, max)| *min + max)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            sums_to_target
+        })
+        .next()
 }
 
 fn parse(s: &str) -> Vec<usize> {
@@ -82,5 +111,16 @@ mod tests {
         ];
 
         assert_eq!(Some(&127), find_preamble_error(&v, 5).first());
+    }
+
+    #[test]
+    fn find_windows_adding_to_test() {
+        let v = vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+
+        let windows_adding_to_127 = find_min_max_sums_in_first_window_adding_to(&v, 127);
+        assert_eq!(Some(62), windows_adding_to_127);
     }
 }
